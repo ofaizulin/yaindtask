a5db9052be6b418fcfe2a42ad18b1c1e4b755b6e
Oleh Faizulin <oleh.faizulin@gmail.com>
2024-06-15 17:48:48 +0300

    Two more word analyzers

---
+++ src/main/java/yaindtask/analyzers/positions/word/PositionOfWordsContainingText.java
@@ -0,0 +1,29 @@
+package yaindtask.analyzers.positions.word;
+
+import yaindtask.analyzers.AnalysisResult;
+import yaindtask.analyzers.Analyzer;
+import yaindtask.analyzers.util.AnalyzerFunctions;
+import yaindtask.tokenizers.word.DefaultWordTokenizer;
+import yaindtask.tokenizers.word.WordTokenizer;
+
+public class PositionOfWordsContainingText implements Analyzer {
+
+  private final WordTokenizer wordTokenizer = new DefaultWordTokenizer();
+  private final String textToContain;
+
+  public PositionOfWordsContainingText(String textToContain) {
+    this.textToContain = textToContain;
+  }
+
+  @Override
+  public AnalysisResult analyze(String text) {
+    var data = wordTokenizer.tokenize(text).stream()
+        .filter(t -> t.text().contains(this.textToContain))
+        .map(t -> new String[]{
+            t.text(),
+            AnalyzerFunctions.tokenPositions(t)
+        })
+        .toList();
+    return AnalysisResult.of("Word", "Positions", data);
+  }
+}
---
+++ src/main/java/yaindtask/analyzers/positions/word/PositionOfWordsEndingWithText.java
@@ -0,0 +1,29 @@
+package yaindtask.analyzers.positions.word;
+
+import yaindtask.analyzers.AnalysisResult;
+import yaindtask.analyzers.Analyzer;
+import yaindtask.analyzers.util.AnalyzerFunctions;
+import yaindtask.tokenizers.word.DefaultWordTokenizer;
+import yaindtask.tokenizers.word.WordTokenizer;
+
+public class PositionOfWordsEndingWithText implements Analyzer {
+
+  private final WordTokenizer wordTokenizer = new DefaultWordTokenizer();
+  private final String textToEndWith;
+
+  public PositionOfWordsEndingWithText(String textToEndWith) {
+    this.textToEndWith = textToEndWith;
+  }
+
+  @Override
+  public AnalysisResult analyze(String text) {
+    var data = wordTokenizer.tokenize(text).stream()
+        .filter(t -> t.text().endsWith(this.textToEndWith))
+        .map(t -> new String[]{
+            t.text(),
+            AnalyzerFunctions.tokenPositions(t)
+        })
+        .toList();
+    return AnalysisResult.of("Word", "Positions", data);
+  }
+}
